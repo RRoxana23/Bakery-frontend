9ca4717e9b5c01423046270eadb5cbe3b8f59ed0
RRoxana23 <roxanaqs90@gmail.com>
2024-03-06 16:44:03 +0200

    My account Page bug solved + regex for the password

--- src/app/components/my-account-page/my-account-page.component.ts
+++ src/app/components/my-account-page/my-account-page.component.ts
@@ -2,23 +2,45 @@ import { Component } from '@angular/core';
 import { MenuBarComponent } from "../menu-bar/menu-bar.component";
 import { PageContentComponent } from "../page-content/page-content.component";
 import { PageContent } from '../../interfaces/page-content';
-import { FormControl, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, Validators, FormsModule, ReactiveFormsModule, FormBuilder, FormGroup, FormGroupDirective, NgForm } from '@angular/forms';
 import { MatInputModule} from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatIconModule } from '@angular/material/icon';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { CommonModule } from '@angular/common';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+    isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+      const isSubmitted = form && form.submitted;
+      return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+    }
+  }
 
 @Component({
     selector: 'app-my-account-page',
     standalone: true,
     templateUrl: './my-account-page.component.html',
     styleUrl: './my-account-page.component.css',
-    imports: [MenuBarComponent, PageContentComponent, MatFormFieldModule, FormsModule, ReactiveFormsModule, MatIconModule, MatInputModule, MatButtonModule, MatCheckboxModule]
+    imports: [MenuBarComponent, PageContentComponent, MatFormFieldModule, FormsModule, ReactiveFormsModule, MatIconModule, MatInputModule, MatButtonModule, MatCheckboxModule, CommonModule]
 })
 export class MyAccountPageComponent {
 
-    email = new FormControl('', [Validators.required, Validators.email]);
+    formGroup: FormGroup;
+    isPasswordInvalid: boolean = false;
+
+    constructor(private builder: FormBuilder) {
+    
+        this.formGroup = this.builder.group({
+          emailFormControl: ['', [Validators.required, Validators.email]],
+          passwordFormControl: ['', [
+            Validators.required, 
+            Validators.pattern(/^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{6,}$/)
+          ]]
+        });
+      }
+
     hide = true;
     signUp : boolean = false;
 
@@ -28,18 +50,12 @@ export class MyAccountPageComponent {
     signUpClicked : boolean = false;
     signedUp : boolean = false;
 
+    matcher = new MyErrorStateMatcher();
+
     myAccount: PageContent = {
         leftPhoto: './assets/myAcc-page-img.jpg',
         title: 'Login'
     };
-
-    getErrorMessageEmail() {
-        if (this.email.hasError('required')) {
-          return 'Email Adress is required';
-        }
-
-        return this.email.hasError('email') ? 'Not a valid email' : '';
-    }
 
     signUpToAppear() {
         this.emailPlaceholder = 'Your email address';
@@ -60,5 +76,13 @@ export class MyAccountPageComponent {
         this.signUpClicked = false;
         this.myAccount.title = this.signUpClicked ? 'Sign Up' : 'Login';
         this.signedUp = false;
+    }
+
+    onPasswordBlur() {
+        if (this.formGroup.get('passwordFormControl')?.hasError('pattern')) {
+          this.isPasswordInvalid = true;
+        } else {
+          this.isPasswordInvalid = false;
+        }
     }
 }
